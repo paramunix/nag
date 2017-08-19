ddc5ab1f23a6d8bac5ca34d8b85436f1af1d1cbf
root <root@dixidee6.mylabserver.com>
2017-08-19 11:15:19 +0000

    first commit

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REPO_PATH="/etc/yum.repos.d"
+
+cp -p ./play/master-repo.repo $REPO_PATH/
+
+
+echo
+rm -rf /var/cache/yum
+yum clean all
+grep "enabled=1" $REPO_PATH/$i
+yum repolist
+echo
+yum install -y ansible
